1d1195b50cd060bba5f0debe266a341c912f6f5c
JerryCoding404 <summer83410@yahoo.com.tw>
2020-05-25 19:53:08 +0800

    Changing musicMP3.py code

--- musicMP3.py
+++ musicMP3.py
@@ -8,4 +8,7 @@ pygame.mixer.init()
 pygame.mixer.music.load("./起來現在也才3點.mp3")
 pygame.mixer.music.play()
 
+#hi this is testing
+#python is a very useful tool for programing AI's 
+
 time.sleep(100)
